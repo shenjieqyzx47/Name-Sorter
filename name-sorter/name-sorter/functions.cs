693bd2936b84acee4c5b0cba8d56072291f4a2cc
shenjieqyzx47 <526553064@qq.com>
2021-06-09 20:50:31 +1000

    name-sort application
    detach functions

--- name-sorter/name-sorter/functions.cs
+++ name-sorter/name-sorter/functions.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.IO;
 
-namespace name_sorter
+namespace NameSorter
 {
     class functions
     {
-        public static List<object> ReadNames(string file_address) {
+        //read names from unsorted text file and put them into a list.
+        public static List<User> ReadNames(string file_address) {
+            List<User> unsorted_name_list = new List<User>();
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(file_address);
+            while ((line = file.ReadLine()) != null)
+            {
+                unsorted_name_list.Add(new User(line));
+            }
+            file.Close();
+            return unsorted_name_list;
+        }
 
-            return null;
+        //sort the unsorted_name_list 
+        public static List<User> SortNames(List<User> unsorted_name_list) {
+            List<User> sorted_name_list = unsorted_name_list.OrderBy(x => x.last_name).ToList();
+            return sorted_name_list;
+        }
+
+        //print the sorted_name_list to screen and write into sorted-names-list.txt
+        public static void WriteNames(List<User> sorted_name_list) {
+            using (StreamWriter target_text_file = new StreamWriter("sorted-names-list.txt"))
+            foreach (User user in sorted_name_list)
+            {
+                System.Console.WriteLine(user.full_name);
+                    target_text_file.WriteLine(user.full_name);
+            }
         }
     }
 }
--- name-sorter/name-sorter/name-sorter.cs
+++ name-sorter/name-sorter/name-sorter.cs
@@ -14,28 +14,11 @@ namespace NameSorter
         {
             foreach (string arg in args)
             {
-                //read the given file and put all names into a list.
-                List<User> users = new List<User>();
-                string line;
-                System.IO.StreamReader file = new System.IO.StreamReader(arg);
-                while ((line = file.ReadLine()) != null)
-                {
-                    users.Add(new User(line));
-                }
-                file.Close();   //end reading txt file.
-                                //sort the list by the given name
-                IEnumerable<User> sorted_users = from user in users
-                                                 orderby user.given_name
-                                                 select user;   //end sorting
-                                                                //print the sorted names as well as write into a new txt file
-                using (StreamWriter sorted_names_list = new StreamWriter("sorted-names-list.txt"))
-                    foreach (User user in sorted_users)
-                    {
-                        System.Console.WriteLine(user.full_name);
-                        sorted_names_list.WriteLine(user.full_name);
-                    }   //end printing and writing
-                System.Console.ReadLine();
+                List<User> unsorted_name_list = functions.ReadNames(arg);
+                List<User> sorted_name_list = functions.SortNames(unsorted_name_list);
+                functions.WriteNames(sorted_name_list);
             }
+            System.Console.ReadLine();
         }
     }
 }
